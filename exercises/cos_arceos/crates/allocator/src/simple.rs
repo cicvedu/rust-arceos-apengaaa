b8238f0edeb1c1622ce62da8679715e92d21ea69
apengaaa <jiangpengzzz@163.com>
2023-08-27 14:45:37 +0800

    new

--- exercises/cos_arceos/crates/allocator/src/simple.rs
+++ exercises/cos_arceos/crates/allocator/src/simple.rs
@@ -1,48 +1,75 @@
-//! Simple memory allocation.
-//!
-//! TODO: more efficient
+// //! Simple memory allocation.
+// //!
+// //! TODO: more efficient
 
 use core::alloc::Layout;
 use core::num::NonZeroUsize;
 
-use crate::{AllocResult, BaseAllocator, ByteAllocator};
+use crate::{AllocError, AllocResult, BaseAllocator, ByteAllocator};
 
-pub struct SimpleByteAllocator;
+pub struct SimpleByteAllocator {
+    start: usize,
+    end: usize,
+    next: usize,
+    allocations: usize,
+}
 
 impl SimpleByteAllocator {
     pub const fn new() -> Self {
-        Self {}
+        Self {
+            start: 0,
+            end: 0,
+            next: 0,
+            allocations: 0,
+        }
     }
 }
 
 impl BaseAllocator for SimpleByteAllocator {
-    fn init(&mut self, _start: usize, _size: usize) {
-        todo!();
+    fn init(&mut self, start: usize, size: usize) {
+        self.start = start;
+        self.end = start + size;
+        self.next = start;
     }
 
-    fn add_memory(&mut self, _start: usize, _size: usize) -> AllocResult {
-        todo!();
+    fn add_memory(&mut self, start: usize, size: usize) -> AllocResult {
+        if self.start == 0 {
+            self.init(start, size);
+            Ok(())
+        } else {
+            Err(AllocError::InvalidParam)
+        }
     }
 }
 
 impl ByteAllocator for SimpleByteAllocator {
-    fn alloc(&mut self, _layout: Layout) -> AllocResult<NonZeroUsize> {
-        todo!();
+    fn alloc(&mut self, layout: Layout) -> AllocResult<NonZeroUsize> {
+        let size = layout.size();
+        if self.next + size > self.end {
+            return Err(AllocError::NoMemory);
+        }
+        let result = self.next;
+        self.next += size;
+        self.allocations += 1;
+        NonZeroUsize::new(result).ok_or(AllocError::InvalidParam)
     }
 
     fn dealloc(&mut self, _pos: NonZeroUsize, _layout: Layout) {
-        todo!();
+        self.allocations -= 1;
+        if self.allocations == 0 {
+            self.next = self.start;
+        }
     }
 
     fn total_bytes(&self) -> usize {
-        todo!();
+        self.end - self.start
     }
 
     fn used_bytes(&self) -> usize {
-        todo!();
+        self.next - self.start
     }
 
     fn available_bytes(&self) -> usize {
-        todo!();
+        self.end - self.next
     }
 }
